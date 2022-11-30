e18f5c4675c8d022107d327adf1849aece213113
prabhat20012001 <prabhatkumarmnnit@gmail.com>
2022-12-01 01:13:50 +0530

    first commit

--- dom.js
+++ dom.js
@@ -1,17 +1,70 @@
-// var item=document.querySelector(".list-group-item:nth-child(2)");
-// item.style.backgroundColor="green" ;
+// traversing the dom
+// var itemList=document.querySelector('#items');
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor='#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
 
-// var item=document.querySelector(".list-group-item:nth-child(3)");
-// item.style.color="white";
 
-// Using QuerySelectorALL change the font color to green for 2nd item in the item list
-// Choose all the odd elements and make their background green using QuerySelectorALL﻿
+// parentElement
+// console.log(itemList.parentElement);
+//  itemList.parentNode.style.backgroundColor='#f4f4f4';
+//  console.log(itemList.parentElement.parentElement.parentElement);
 
-var item=document.querySelectorAll(".list-group-item");
-item[1].style.color="green";
 
-// var odd=document.querySelectorAll('li:nth-child(odd)')
-// for(var i=0;i<odd.length;i++)
-// {
-//     odd[i].style.background="black";
-// }
+
+// child nodes
+
+// console.log(itemList.childNodes)
+
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor='yellow';
+
+// firstChild
+// console.log(itemList.firstChild);
+
+// firstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent='Hello 1'
+
+// lASTChild
+// console.log(itemList.lastChild)
+// lastElement
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent="Hello 4"
+
+// nextSibling
+// console.log(itemList.nextSibling)
+// // nextElementsibling
+// console.log(itemList.nextElementSibling);
+
+// previousSibling
+// console.log(itemList.previousSibling);
+// // previousElementsibling
+// console.log(itemList.previousElementSibling)
+// itemList.previousElementSibling.style.color="red";
+
+// create element 
+
+
+// create a div
+var newDiv=document.createElement("div");
+// add class
+newDiv.className="hello";
+
+// add id
+newDiv.id="hello1";
+
+// add attr
+newDiv.setAttribute("title","Hello div");
+// create text node
+var newDivText=document.createTextNode("Hello world");
+
+// Add text to div
+newDiv.appendChild(newDivText);
+var container=document.querySelector("header .container");
+var h1=document.querySelector("header h1");
+console.log(newDiv);
+newDiv.style.fontSize='30px';
+
+container.insertBefore(newDiv,h1);
